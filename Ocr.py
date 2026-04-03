3f9fd560d01b5309a271f7c0f8eef6aef9dc64b7
manueljpc <jeremypilier1@gmail.com>
2026-04-03 18:31:54 -0400

    me esta dando error easyocr

--- Ocr.py
+++ Ocr.py
@@ -1,3 +1,22 @@
 import easyocr
 import numpy as np
 from PIL import Image
+
+#para cargar la imagen
+image = Image.open("prueba_ocr/prueba_ocr.png")
+
+#para convertir en array
+image_np = np.array(image)
+
+
+#cargar modelo
+reader = easyocr.Reader(['es', 'en'], gpu= False)
+
+#detectar texto
+result = reader.readtext(image_np)
+
+#mostrar texto
+
+text = " ".join([r[1] for r in result])
+print("texto detectado")
+print(text)
