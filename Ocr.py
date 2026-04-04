4a6c92eb70cb3f1ac0f6342f02c32d1aea031372
manueljpc <jeremypilier1@gmail.com>
2026-04-03 21:02:43 -0400

    el problema parece ser la calidad de la foto, por lo que para que funcione las fotos tienen que ser claras, no estar borrosas, ni estar mal encuadradas

--- Ocr.py
+++ Ocr.py
@@ -1,15 +1,31 @@
 import pytesseract
+import cv2
 from PIL import Image
 
 
 try: 
     
     #para cargar la imagen
-    image = Image.open("prueba_ocr/prueba_ocr3.jpg")
+    image = cv2.imread("prueba_ocr/prueba_ocr2.png")
+    
+    if image is None:
+        raise Exception("no se pudo cargar la imagen")
+    
+    
+    #para convertir a gris 
+    gray = cv2.cvtColor (image, cv2.COLOR_BGR2GRAY)
+    
+    
 
+    #mejora el ocr
+    
+    _, thresh = cv2.threshold(gray,150,255,cv2.THRESH_BINARY)
+    
+    image_pil = Image.fromarray(thresh)
+    
 
     #cargar modelo
-    text = pytesseract.image_to_string(image, lang = 'spa')
+    text = pytesseract.image_to_string(thresh, lang = 'spa', config='--psm 6')
 
     #mostrar texto
 
