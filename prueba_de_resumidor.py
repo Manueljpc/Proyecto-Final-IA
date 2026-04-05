aa559b6a432bc323b66b114df87601d5fd248cdb
manueljpc <jeremypilier1@gmail.com>
2026-04-04 20:19:01 -0400

    el problema era una libreria que impedia el que se resumiera el texto

--- prueba_de_resumidor.py
+++ prueba_de_resumidor.py
@@ -1,5 +1,7 @@
 from Resumen import resumir_texto
 
-texto = """El deep learning o aprendizaje profundo es una rama especializada del machine learning y la inteligencia artificial que utiliza redes neuronales artificiales con"""
+texto = """El machine learning o aprendizaje automático es una rama de la inteligencia artificial que permite a las computadoras aprender y mejorar su rendimiento en una 
+tarea específica a partir de datos y experiencia, sin necesidad de ser programadas explícitamente para ello.  Esta tecnología se centra en el uso de algoritmos para identificar 
+patrones en grandes volúmenes de información, lo que facilita la realización de predicciones, clasificaciones y recomendaciones precisas"""
 
 print(resumir_texto(texto))
