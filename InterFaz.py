3c625ba6671f598737f98ee3b3868bb9644255c7
manueljpc <jeremypilier1@gmail.com>
2026-04-05 19:08:26 -0400

    estoy intentado poner un boton extra que me permita limpiar los cuadros donde esta el texto detectado y el resumen

--- InterFaz.py
+++ InterFaz.py
@@ -20,20 +20,31 @@ def procesar_imagenes(imagen_ruta):
     resultado_resumen = resumir_texto(texto_extraido)
     return texto_extraido , resultado_resumen
     
+    #limpiar campos
+def limpiar_campos():
+    return None , ","
     
 # interfaz grafica
 
-with gr.Blocks(titulo = "SmartNote AI") as interfaz:
+with gr.Blocks(title = "SmartNote AI") as interfaz:
+    gr.Markdown("SmartNote")
     gr.Markdown("sube una foto o imagen de tus apuntes para resumirlos")
     
     #entrada de imagen
-with gr.Row.Column():
-    subir_imagen = gr.Image(type="filepath", label="foto de tus apuntes")
-    boton_ejecutar = gr.Button("procesar imagenes", variant="primary")    
+    with gr.Row():
+    
+        with gr.Column():
+            subir_imagen = gr.Image(type="filepath", label="foto de tus apuntes", sources=["upload"])
+            boton_ejecutar = gr.Button("procesar imagenes", variant="primary")    
+            
+            with gr.Row():
+                boton_limpiar = gr.Button("cerrar/limpiar", variant="secondary")
+                boton_ejecutar = gr.Button("procesar notas",variant="primary")
+            
     #SALIDA DE texto
-with gr.Column():
-    salida_texto = gr.Textbox(label = "texto identificado", lines=8)
-    salida_resumen = gr.Textbox(label="resumen", lines=5)
+        with gr.Column():
+            salida_texto = gr.Textbox(label = "texto identificado", lines=8)
+            salida_resumen = gr.Textbox(label="resumen", lines=5)
     
     # accion al hacer click al boton   
     boton_ejecutar.click(
@@ -41,3 +52,13 @@ with gr.Column():
         inputs=subir_imagen,
         outputs=[salida_texto,salida_resumen]
     )
+    
+    #accion para limpiar
+    
+    boton_limpiar.click(
+        fn=limpiar_campos,
+        inputs = []
+        outputs = [subir_imagen , salida_texto ,salida_resumen]
+    )
+if __name__ == "__main__":
+    interfaz.launch()
