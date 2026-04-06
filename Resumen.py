ed8154f7b5ef7823127d85b507fe528ebfcf8ba7
manueljpc <jeremypilier1@gmail.com>
2026-04-05 23:06:21 -0400

    al final me quede con el modelo t5-small, ya que los otros al ser tan pesados hacian que se me cerrara el vscode, este me funciona bien, los resumenes son cortos per funciona

--- Resumen.py
+++ Resumen.py
@@ -1,31 +1,30 @@
 from transformers import pipeline
+import gc
 
 #para cargar el modelo
-resumidor = pipeline("summarization", model = "google/mt5-small")
+resumidor = pipeline("summarization", model = "t5-small")
 
 def resumir_texto(texto):
-    if not texto or len(texto) < 50:
+    if not texto or len(texto.strip()) < 30:
         return "el texto es demasiado corto"
     
     
     try:
-        
+        #para que el modelo entienda lo que tiene que hacer
         entrada_texto = "summarize:" + texto
         
     #para generar el resumen
         resumen = resumidor(
         
         entrada_texto,
-        max_length = 300,
-        min_length = 80,
-        do_sample = True,
-        top_k = 50,
-        top_p = 0.90, 
-        punctuation_penalty = 1.0,
-    
-        
+        max_length = 150,
+        min_length = 40,
+        do_sample = False,    
         )
     
+        gc.collect()
+        
+        
         return resumen[0]['summary_text']
     
     except Exception as e:
